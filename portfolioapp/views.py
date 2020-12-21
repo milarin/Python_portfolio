06badcc9ceb86722a8c8ce3c4f3cb626a8615356
milan <you@example.com>
2020-12-21 21:48:24 +0900

    first commit

---
+++ portfolioapp/views.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.http.response import HttpResponse
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import WorkModel
+
+# Create your views here.
+
+class WorkList(ListView):
+  template_name = 'list.html'
+  model = WorkModel
+
+class WorkDetail(DetailView):
+  template_name = 'detail.html'
+  model = WorkModel
+
+def introduce(request):
+  return render(request, 'intro.html', {})
